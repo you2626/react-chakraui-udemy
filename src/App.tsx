58589173a7fd715fb46624ad856f402fcfea1f95
KURUMI <k.1au12um1@gmail.com>
2024-09-15 23:48:27 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import { Button,ChakraProvider } from '@chakra-ui/react'
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <ChakraProvider>
+      <Button colorScheme="teal">ボタン</Button>
+     
+    </ChakraProvider>
+  )
+}
+
+export default App
